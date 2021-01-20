015b7b02655b9da53a682fe05e759d8b473a0ca5
12emin34 <43503468+12emin34@users.noreply.github.com>
2021-01-20 15:06:34 +0100

    Prvi commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function upload() {
+    document.getElementById("uspijeh").innerHTML = "Successful!";
+    alert("Ime maila: lukalatin11@hotmail.com");
+    window.location = "https://mail.google.com/mail/u/0/#drafts?compose=new";
+}
+
+function webshop() {
+    document.getElementById("message").innerHTML = "Successful!";
+    window.location = "shop.html";
+}
